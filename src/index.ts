aff762b089bd4a481487e2b815b15bdb9348a7dd
MohamedElnadry1 <60830713+MohamedElnadry1@users.noreply.github.com>
2022-11-22 18:23:14 +0200

    edit erros

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express from "express";
+import express, { Request, Response } from "express";
 const app = express();
 const port = 3000;
 import routes from "./routes/routes";
 app.use(express.static(process.cwd() + "/images/"));
 app.use(express.static(process.cwd() + "/images/imgeAfteEdit"));
 
-app.get("/", (req, res) => {
-  res.send("bad request try /api");
+app.get("/", (req: Request, res: Response): void => {
+  res.send("Bad Request try /api");
 });
 app.get("/api", routes);
 
